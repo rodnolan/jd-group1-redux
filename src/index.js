44bc8192be515a5d87361e570bec3fe07674a548
Rod Nolan <rodn@dev6.com>
2020-01-28 13:22:05 -0600

    create and wire up store to App component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer1 from './redux/reducers/reducer1';
+const store = createStore(reducer1);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
