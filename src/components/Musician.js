bf692198ff7173bc5abbccc9a59af9d0bdb12015
Rod Nolan <rodn@dev6.com>
2020-01-28 15:56:12 -0600

    from the dumb component, call the action creator function that was passed as a prop

--- src/components/Musician.js
+++ src/components/Musician.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Musician = ({name}) => {
-  return <p>{name}</p>;
+const Musician = ({name, updateMusician}) => {
+  return <> 
+    <p>{name}</p>
+    <button onClick={ 
+      () => {
+        console.log('about to dispatch a request to update the musician name in response to a button click');
+        updateMusician("John Lennon");
+      }
+    }>update Bob</button>
+  </>;
 }
 
 
