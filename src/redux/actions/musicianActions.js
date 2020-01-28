ed2907414cded18cc8ce0a30dcb17041b1846683
Rod Nolan <rodn@dev6.com>
2020-01-28 15:56:27 -0600

    debugging

--- src/redux/actions/musicianActions.js
+++ src/redux/actions/musicianActions.js
@@ -1,6 +1,7 @@
 export const UPDATE_MUSICIAN = 'UPDATE_MUSICIAN';
 
 export function updateMusicianAction( musicianName ) {
+  console.log('action creator executed');
   return {
     type: UPDATE_MUSICIAN,
     payload: { name: musicianName }
