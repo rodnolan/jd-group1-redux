a8d4fddc1aaf2f962e5d889e744904f8a9cbd52b
Rod Nolan <rodn@dev6.com>
2020-01-29 12:50:14 -0600

    generate a new object for state and return it

--- src/redux/reducers/musicianReducer.js
+++ src/redux/reducers/musicianReducer.js
@@ -1,7 +1,28 @@
+import { UPDATE_MUSICIAN } from './../actions/musicianActions';
+
 const defaultState = {
   name: 'Bob Dylan'
 }
 
-export default function musicianReducer(state = defaultState) {
-  return {...state};
+export default function musicianReducer(state = defaultState, action) {
+
+  if (action !== undefined && action.payload !== undefined) {
+    console.log('musicianReducer just received an action');
+    // console.log(action.type);
+    console.log(action.payload);
+  }
+
+  switch (action.type) {
+    case UPDATE_MUSICIAN :
+      const newMusician = {
+        ...state, 
+        ...action.payload
+      };
+      console.log(newMusician);
+      return newMusician;
+
+    default: 
+      return {...state};
+  }
+
 }
